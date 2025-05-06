b11892320f501fd6af31ea47e687705434e60ae1
hamed.makian <hamed.makian@gmail.com>
2025-05-06 23:06:13 +0300

    evaluation and DVC added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-#from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
@@ -61,8 +61,7 @@ except Exception as e:
         raise e
 
 
-
-'''
+print("🚀 Running EvaluationPipeline.main()")
 
 STAGE_NAME = "Evaluation stage"
 try:
@@ -75,5 +74,11 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+'''
+write below in your shell
+set MLFLOW_TRACKING_USERNAME=hamedmakian2018
+set MLFLOW_TRACKING_PASSWORD=66ba938aeda0c5830ddc93215c7f0f744fe89f66
+python main.py
 
 '''
